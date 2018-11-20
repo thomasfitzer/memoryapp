0a0a6e6b51854ca32d636858ec3a4fe4a48357b3
hockey6409 <thomasfitzer89@gmail.com>
2018-11-20 09:50:01 -0600

    Updates

---
+++ precache-manifest.3e1d48171bb0dfadeac4381c224a812a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "babb5d3bc8b8b356ebe5",
+    "url": "/memoryapp/static/js/runtime~main.babb5d3b.js"
+  },
+  {
+    "revision": "a56e82a3d223ab022d01",
+    "url": "/memoryapp/static/js/main.a56e82a3.chunk.js"
+  },
+  {
+    "revision": "8fdf2345b7a4e11badcc",
+    "url": "/memoryapp/static/js/1.8fdf2345.chunk.js"
+  },
+  {
+    "revision": "a56e82a3d223ab022d01",
+    "url": "/memoryapp/static/css/main.f63544e9.chunk.css"
+  },
+  {
+    "revision": "fec5a95e657a1a0d311be840f70f82d0",
+    "url": "/memoryapp/index.html"
+  }
+];
